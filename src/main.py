869e6e77198db3db3591f50ee9c90d7b1a53bb2c
HackTestes <caio.hacktestes@gmail.com>
2026-04-01 16:52:53 -0300

    Added rany2's audio provider command and added help text

--- src/main.py
+++ src/main.py
@@ -8,22 +8,34 @@ import helpers
 
 # AudioProviders
 languages = ["pt-BR", "en-US"]
+
+# Audio provider just for testing (Windows only)
 powershell_test_audio_provider = audio_providers.AudioProvider({
         "pt-BR": "powershell -c 'echo pt-BR [input_file_path] [output_file_path]'",
         "en-US": "powershell -c 'echo en-US [input_file_path] [output_file_path]'"
     },
     3,
+    False
+)
+
+# https://github.com/rany2/edge-tts
+rany2_edge_tts_audio_provider = audio_providers.AudioProvider({
+        "pt-BR": "bash -c \"echo 'edge-tts --voice pt-BR-FranciscaNeural --file [input_file_path] --write-media /tmp/[output_file_path] && mv /tmp/[output_file_path] ./\"",
+        "en-US": "bash -c \"echo 'edge-tts --voice en-US-AndrewNeural --file [input_file_path] --write-media /tmp/[output_file_path] && mv /tmp/[output_file_path] ./\""
+    },
+    3,
     True
 )
+
 audio_providers_per_lang = {"pt-BR": powershell_test_audio_provider, "en-US": powershell_test_audio_provider}
 
 # Get the file program arguments
 parser = argparse.ArgumentParser(description="Manages automatically audio generation services/providers")
-parser.add_argument('--text-path', required=True, type=pathlib.Path)
-parser.add_argument('--hash-path', required=True, type=pathlib.Path)
-parser.add_argument('--ignore-hashes', required=False, default=False, action='store_true')
-parser.add_argument('--ignore-audio-files', required=False, default=False, action='store_true')
-parser.add_argument('--retry', required=False, type=int, default=12)
+parser.add_argument('--text-path', required=True, type=pathlib.Path, help="Path to the folder containing .txt files for processing")
+parser.add_argument('--hash-path', required=True, type=pathlib.Path, help="Path to the hash file (it is where we store the hashes of previous processed files, so we can process only the changed ones)")
+parser.add_argument('--ignore-hashes', required=False, default=False, action='store_true', help="Skip the hash check (aka consider every file as changed)")
+parser.add_argument('--ignore-audio-files', required=False, default=False, action='store_true', help="Do not skip files that already have a audio file")
+parser.add_argument('--retry', required=False, type=int, default=2, help="How many times we shloud retry a command before consider it failed")
 parser.add_argument('--polling-rate', required=False, type=int, default=5, help="Polling interval in seconds") # Seconds
 
 args = parser.parse_args()
