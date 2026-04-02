4a515608ea83fac32e1b968e21154a68169ca98e
HackTestes <caio.hacktestes@gmail.com>
2026-04-02 12:03:39 -0300

    Updated the edge-tts command (mkdir in tmp and explanation in commands)

--- src/main.py
+++ src/main.py
@@ -28,9 +28,13 @@ bash_test_audio_provider = audio_providers.AudioProvider({
 )
 
 # https://github.com/rany2/edge-tts
+# Command explanation:
+#    mkdir: create the corresponding filesystem structure of the texts in the tmp folder
+#    edge-tts: create the audio files in the tmp folder at the equivalent folder
+#    mv: after a successful creation, move the file from the tmp (RAM backed) to the persistent storage alongside the input file
 rany2_edge_tts_audio_provider = audio_providers.AudioProvider({
-        "pt-BR": "bash -c \"echo 'edge-tts --voice pt-BR-FranciscaNeural --file [input_file_path] --write-media /tmp/[output_file_path] && mv /tmp/[output_file_path] ./\"",
-        "en-US": "bash -c \"echo 'edge-tts --voice en-US-AndrewNeural --file [input_file_path] --write-media /tmp/[output_file_path] && mv /tmp/[output_file_path] ./\""
+        "pt-BR": "bash -c \"echo 'mkdir -p /tmp/[input_file_parent] && edge-tts --voice pt-BR-FranciscaNeural --file [input_file_path] --write-media /tmp/[input_file_parent]/[input_file_stem].mp3 && mv /tmp/[input_file_parent]/[input_file_stem].mp3 [input_file_parent]/\"",
+        "en-US": "bash -c \"echo 'mkdir -p /tmp/[input_file_parent] && edge-tts --voice en-US-AndrewNeural --file [input_file_path] --write-media /tmp/[input_file_parent]/[input_file_stem].mp3 && mv /tmp/[input_file_parent]/[input_file_stem].mp3 [input_file_parent]/\""
     },
     3,
     True
